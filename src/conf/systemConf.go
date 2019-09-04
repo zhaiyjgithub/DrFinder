9e5b8831147c99d01d12efd4cfbc639e09376902
zhaiyuanji <chuck.zhaiyj@gmail.com>
2019-09-04 12:34:41 +0800

    delete conf .

--- src/conf/systemConf.go
+++ src/conf/systemConf.go
@@ -4,7 +4,7 @@ package conf
 var (
 	JWRTSecret = []byte("JWRTSecret")
 	ServerEmail = "chuck.zhaiyj@gmail.com"
-	ServerEmailPwd = "fuycnxrhjjxbqvhe"
+	ServerEmailPwd = ""
 	Smtp = "smtp.gmail.com"
 	TimeFormat = "2006-01-02 15:04:05"
 	)
